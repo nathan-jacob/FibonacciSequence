05a4bac43cf3635522def83a20db98a61c8e6b92
Nathan Jacob <37455968+nathan-jacob@users.noreply.github.com>
2019-08-16 15:39:58 -0500

    Update what.py

--- what.py
+++ what.py
@@ -1,4 +1,5 @@
 x = 0
+#list with the sequence
 fib = [0,1]
 while True:
     if (x==0):
@@ -6,6 +7,7 @@ while True:
     if (x==1):
         print("1")
     else:
+        #appends the list with the previous two values
         fib.append(fib[len(fib)-1]+fib[len(fib)-2])
         print(fib[len(fib)-1])
     x = x + 1
